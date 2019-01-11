5a6d061a5bd68ee25c38d308ab2052165f183288
Nicolas <nicobaudon01@gmail.com>
2019-01-11 13:57:52 -0300

    Commit proyecto en blanco-v1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('GIRSU.ico') }}">
         <title>Congreso GIRSU II</title>
 
         <!-- Fonts -->
