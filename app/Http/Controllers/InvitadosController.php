2a5c1bd08ae2f68439b995bd3238e6b88954eab5
Mariano Lopez <marianolopezazeglio@gmail.com>
2019-01-16 03:23:55 -0300

    limpie la vista de invitados y pase la logica al controlador

--- app/Http/Controllers/InvitadosController.php
+++ app/Http/Controllers/InvitadosController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class InvitadosController extends Controller
 {
    public function index(){
-       return view('invitados');
-   }
+       require '..\excelData.php';
+       $invitados = $listFeedInvitados->getEntries();
+       //dd(sizeof($invitados));
+      return view('invitados',compact('invitados'));     
+    }
 }
+
--- resources/views/invitados.blade.php
+++ resources/views/invitados.blade.php
@@ -5,43 +5,33 @@
 <h1>
 INVITADOS
 </h1>
-</center>
-<?php
-require '../vendor/autoload.php';
-require '../excelData.php' //CODIGO QUE TRAE LA DATA DEL EXCEL;
-?>
-<center>
+
 <table border=1>
     <th>NOMBRE</th>
     <th>APELLIDO</th>
-    <th>MAIL</th>
-    <th>TIPO</th>
-    <th>TELEFONO</th>
     <th>DNI</th>
+    <th>EMAIL</th>
+    <th>TELEFONO</th>
+    <th>TIPO</th>
     <th>COMPAÑIA</th>
     <th>TRABAJO</th>
-    <th colspan=2>IMPRIMIR</th>
-    
+    <th colspan=2>IMPRIMIR</th> 
+    @foreach($invitados as $invitado)
+
+    <?php $dato=$invitado->getValues()?>
     <tr>
-        <?php
-        /** @var ListEntry */
-foreach ($listFeedInvitados->getEntries() as $entry) {
-    $representative = $entry->getValues();
-        ?>
-        <td><?php echo $representative['firstname'] ?></td>
-        <td><?php echo $representative['lastname'] ?></td>
-        <td><?php echo $representative['email'] ?></td>
-        <td><?php echo $representative['tickettype'] ?></td>
-        <td><?php echo $representative['cellphone'] ?></td>
-        <td><?php echo $representative['dni'] ?></td>
-        <td><?php echo $representative['company'] ?></td>
-        <td><?php echo $representative['jobtitle'] ?></td>
-        <td><button>CREDENCIAL</button></td>
-        <td><button>CERTIFICADO</button></td>
+    <td>{{$dato['firstname']}}</td> 
+    <td>{{$dato['lastname']}}</td>    
+    <td>{{$dato['dni']}}</td>    
+    <td>{{$dato['email']}}</td>    
+    <td>{{$dato['cellphone']}}</td>    
+    <td>{{$dato['tickettype']}}</td> 
+    <td>{{$dato['company']}}</td>  
+    <td>{{$dato['jobtitle']}}</td>
+    <td><button>CREDENCIAL</button></td>
+    <td><button>CERTIFICADO</button></td>      
     </tr>
-    <?php
-    }
-    ?>
+    @endforeach
+   
 </table>
-</center>
 @endsection
